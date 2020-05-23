2ac2a1242b60254dec142d4a92268042490bcfbc
Melbz <a.mellor91@gmail.com>
2020-05-23 12:18:16 +0100

    changed spotlight colour to gold

--- settings.js
+++ settings.js
@@ -4,8 +4,8 @@ const twitchChannel = 'PeachyMelbz';
 // Your alert background. Default is a vibrant green
 const alertBg = '#00AA00';
 
-// Spotlight background colour. Default is a fiery, fiery orange.
-const spotlightBg = '#ff6ec7';
+// Spotlight background colour. Default is a deep, rich "gold"
+const spotlightBg = '#a66600';
 
 // The emoji that surrounds the spotlight messages.
 const spotlightEmoji = '⭐';
