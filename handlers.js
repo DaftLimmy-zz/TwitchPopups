109d29833adccea0ba10ccce1ad3a8e192dca21c
Jonny Buchanan <jonathan.buchanan@gmail.com>
2020-05-26 19:17:43 +1000

    Scroll long messages Use CSS transitions to show messages Fix for !spotlight missing @ bug (closes #5)

--- handlers.js
+++ handlers.js
@@ -39,7 +39,7 @@ actionHandlers['!spotlight'] = {
         return context.mod || (context["badges-raw"] != null && context["badges-raw"].startsWith("broadcaster"))
     },
     handle: (context, textContent) => {
-        spotlightUser = textContent.substr(12).toLowerCase();
+        spotlightUser = textContent.replace(/!spotlight @?/, '').toLowerCase();
         popup.showText(`${spotlightEmoji} Welcome ${spotlightUser} to the stage!`, spotlightBg);
     }
 };
--- popup-helper.js
+++ popup-helper.js
@@ -1,25 +1,97 @@
+// Pixels per second
+const scrollSpeed = 300;
+// Delay in seconds before scrolling starts
+const scrollStartDelay = 2;
+// Delay in seconds after scrolling ends
+const scrollEndDelay = 3;
+// Time taken in seconds for text to fade in and out
+// Must be less than scrollStartDelay and scrollEndDelay
+const scrollTextFadeTime = 0.5;
+
+const roundTenth = (num) => Math.round(num * 10) / 10;
+
+const popupScrollStyle = document.createElement('style');
+document.head.appendChild(popupScrollStyle);
+
 const popup = {
     /**
      * Displays popup on screen with the given text and colour.
+     *
+     * If the text causes the popup to overflow, it will be scrolled.
      */
     showText: (text, bgColour) => {
-        $("#popupbox").show();
-        $("#popuptext").html(text);
-        $("#popupbox").css({ "background-color": bgColour });
-        $("#popuptext").css({ "opacity": 0, "margin-left": "50px" });
+        const popupBox = document.getElementById('popupbox');
+        const popupText = document.getElementById('popuptext');
+        popupText.innerHTML = text;
 
-        const textWidth = $("#popuptext").width();
-        $("#popupbox").width(1);
-        $("#popupbox").animate({ width: textWidth + 30 }, 500);
-        $("#popuptext").animate({ "opacity": 1, "margin-left": "15px" }, 700);
+        // Set the background colour on the text if it's not overflowing
+        if (popupText.offsetWidth <= popupBox.offsetWidth) {
+            popupBox.style.backgroundColor = 'transparent';
+            popupText.style.backgroundColor = bgColour;
+            popupText.style.animation = 'none';
+            popup._animate();
+            return;
+        }
+
+        const overflow = popupText.offsetWidth - popupBox.offsetWidth
+        const animationTime =  roundTenth(scrollStartDelay + (overflow / scrollSpeed) + scrollEndDelay);
+        const textFadeInPercent = roundTenth(scrollTextFadeTime / animationTime * 100);
+        const startScrollingPercent = roundTenth(scrollStartDelay / animationTime * 100);
+        const stopScrollingPercent = 100 - roundTenth(scrollEndDelay / animationTime * 100);
+        const animationName = `scroller-${Date.now()}`
+
+        if (popupScrollStyle.sheet.cssRules.length > 0) {
+            popupScrollStyle.sheet.deleteRule(0);
+        }
+        popupScrollStyle.sheet.insertRule(`@keyframes ${animationName} {
+            0% {
+                transform: translate3d(0, 0, 0);
+                opacity: 0;
+            }
+            ${textFadeInPercent}% {
+                opacity: 1;
+            }
+            ${startScrollingPercent}% {
+                transform: translate3d(0, 0, 0);
+            }
+            ${stopScrollingPercent}% {
+                transform: translate3d(-${overflow}px, 0, 0);
+            }
+            ${100 - textFadeInPercent}% {
+                opacity: 1;
+            }
+            100% {
+                transform: translate3d(-${overflow}px, 0, 0);
+                opacity: 0;
+            }
+        }`, 0);
+
+        popupBox.style.backgroundColor = bgColour;
+        popupText.style.backgroundColor = 'transparent';
+        popupText.style.animation = `${animationName} ${animationTime}s linear infinite`;
+        popup._animate();
     },
     /**
-     * Removes popup from screen and resets state of all commands 
+     * Animates display of the popup.
+     */
+    _animate: () => {
+        const popupBox = document.getElementById('popupbox');
+        if (popupBox.classList.contains('show')) {
+            // Hide without animation so the show animation can be replayed.
+            popupBox.style.transition = 'none';
+            popupBox.classList.remove('show');
+            // Trigger reflow to apply the style changes above
+            void popupBox.offsetWidth;
+            popupBox.style.transition = '';
+        }
+        popupBox.classList.add('show');
+    },
+    /**
+     * Removes popup from screen and resets state of all commands
      */
     delete: () => {
         spotlightUser = ""; // TODO: Remove this
-        $("#popupbox").animate({ width: 0 }, 500);
-        $("#popuptext").animate({ "opacity": 0, "margin-left": "50px" }, 700);
+        document.getElementById('popupbox').classList.remove('show');
     },
     /**
      * Formats text with emotes, This must be past only and all message un-formatted or emotes wont be replaced properly
