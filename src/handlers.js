180e76e4ce3ee47740fb2ec9e8d331fb473b28bb
Callum Taylor <callum@callumtaylor.net>
2020-10-20 14:28:41 +0100

    Formatting

--- src/handlers.js
+++ src/handlers.js
@@ -38,10 +38,10 @@ actionHandlers['!amazon'] = {
         return context.mod || (context["badges-raw"] != null && context["badges-raw"].startsWith("broadcaster"))
     },
     handle: (context, textContent) => {
-		var text = "gaming.amazon.com"
-		var emote = "imGlitch"
-		var emotePositions = {112290: [`0-${emote.length - 1}`, `${text.length + emote.length + 2}-${text.length + (emote.length + emote.length + 2)}`]}
-		const formattedText = popup.formatEmotes(`${emote} ${text} ${emote}`, emotePositions, true);
+        var text = "gaming.amazon.com"
+        var emote = "imGlitch"
+        var emotePositions = {112290: [`0-${emote.length - 1}`, `${text.length + emote.length + 2}-${text.length + (emote.length + emote.length + 2)}`]}
+        const formattedText = popup.formatEmotes(`${emote} ${text} ${emote}`, emotePositions, true);
         popup.showText(formattedText, alertBg);
     }
 };
